53c982822850f829ead37fd68e99b29201bb0579
alexandrebatista <alexandre.lubasi@gmail.com>
2021-03-26 21:51:22 -0300

    Improving code

--- index.js
+++ index.js
@@ -22,30 +22,21 @@ function sleep(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function switchRectangles(selected_element_id, compared_element_id) {
-	if (parseInt(selected_element_id) <= parseInt(compared_element_id)) {
-		document.getElementById(compared_element_id).className +=
-			' compared_element';
-
+function switchRectangles(selected_element, compared_element) {
+	if (parseInt(selected_element.id) <= parseInt(compared_element.id)) {
+		compared_element.className += ' compared_element';
 		return false;
 	} else {
-		document.getElementById(compared_element_id).className +=
-			' disordered_element';
+		compared_element.className += ' disordered_element';
 
-		let tmp_id = compared_element_id;
-		let tmp_height = document.getElementById(compared_element_id).style
-			.height;
+		let tmp_id = compared_element.id;
+		let tmp_height = compared_element.style.height;
 
-		document.getElementById(
-			compared_element_id
-		).style.height = document.getElementById(
-			selected_element_id
-		).style.height;
-		document.getElementById(compared_element_id).id = selected_element_id;
-		a = tmp_height;
+		compared_element.style.height = selected_element.style.height;
+		compared_element.id = selected_element.id;
 
-		document.getElementById(selected_element_id).style.height = tmp_height;
-		document.getElementById(selected_element_id).id = tmp_id;
+		selected_element.style.height = tmp_height;
+		selected_element.id = tmp_id;
 
 		return true;
 	}
@@ -70,47 +61,24 @@ function generateRandomArray(size) {
 
 async function bubbleSort() {
 	let rectangles_array = document.getElementsByClassName('rectangle');
-	let switched = true;
 
-	while (switched) {
-		switched = false;
+	for (let i = 0; i < rectangles_array.length; i++) {
+		let selected_element = rectangles_array[i];
+		let selected_element_oldclass = selected_element.className;
 
-		for (let i = 0; i < rectangles_array.length; i++) {
-			let rectangles_array = document.getElementsByClassName('rectangle');
+		selected_element.className += ' selected_element';
 
-			let selected_element_id = rectangles_array[i].id;
-			let selected_document_class = rectangles_array[i].className;
+		for (let j = i + 1; j < rectangles_array.length; j++) {
+			let compared_element = rectangles_array[j];
+			let compared_element_oldclass = compared_element.className;
 
-			document.getElementById(selected_element_id).className +=
-				' selected_element';
+			switchRectangles(selected_element, compared_element);
 
-			for (let j = i + 1; j < rectangles_array.length; j++) {
-				let compared_element_id = rectangles_array[j].id;
-				let compared_document_class = rectangles_array[j].className;
-
-				switched = switchRectangles(
-					selected_element_id,
-					compared_element_id
-				);
-				a = document.getElementById('speed_slider');
-
-				await sleep(
-					parseInt(document.getElementById('speed_slider').value)
-				);
-				if (switched) {
-					tmp_id = compared_element_id;
-					compared_element_id = selected_element_id;
-					selected_element_id = tmp_id;
-				}
-
-				document.getElementById(
-					compared_element_id
-				).className = compared_document_class;
-			}
-
-			document.getElementById(
-				selected_element_id
-			).className = selected_document_class;
+			await sleep(
+				parseInt(document.getElementById('speed_slider').value)
+			);
+			compared_element.className = compared_element_oldclass;
 		}
+		selected_element.className = selected_element_oldclass;
 	}
 }
